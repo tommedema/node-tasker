0075c6db5e46a14bbc5d2c3d762c1738378eafd5
tommedema <tommedema@gmail.com>
2011-09-23 23:39:47 +0200

    implemented scheduler logic

--- src/scheduler.js
+++ src/scheduler.js
@@ -7,7 +7,7 @@ var getTasksRunning = exports.getTasksRunning = function() {
     var tasker = this;
     
     /* return amount */
-    return tasker.tasksRunning;
+    return tasker.scheduler.runningTasks;
 };
 
 /* TaskScheduler */
@@ -16,8 +16,11 @@ var TaskScheduler = exports.TaskScheduler = function(tasker) {
     /* save reference to tasker */
     this.tasker = tasker;
     
-    /* set interval */
-    this.interval = setInterval(this.checkQueue, tasker.options.pollRate);
+    /* default to 0 running tasks */
+    this.runningTasks = 0;
+    
+    /* check queue */
+    this.checkQueue();
 };
 
 /* checks the queue for possibly needed activity */
@@ -28,32 +31,38 @@ TaskScheduler.prototype.checkQueue = function() {
         tasker      = scheduler.tasker;
     
     /* check if queue is populated */
-    if (!tasker.getTasksQueued()) return;
+    if (!tasker.getTasksQueued()) return checkQueueSoon();
     
     /* check CPU, MEM and TasksRunning */
-    if (!this.shouldExecTask()) return;
+    if (!this.shouldExecTask()) return checkQueueSoon();
     
     /* call callback of first task, pass checkNext and onDone */
-    var task  = tasker.queue.getFromIndex(0);
+    var task = tasker.queue.getFromIndex(0);
     
     /* function to be called to check next task */
     var checkNext = function() {
         
+        /* check the queue again */
+        this.checkQueue();
     };
     
     /* function to be called when task finishes */
     var onDone = function() {
         
+        /* update running tasks */
+        scheduler.runningTasks--;
+        
+        /* task done */
+        tasker.emit('taskDone', task);
     };
     
     /* execute task */
     this.executeTask(task, checkNext, onDone);
-    
-    /* clear interval until checkNext is called */
-    clearInterval(scheduler.interval);
-    
-    /* update running tasks on onDone */
-    
+};
+
+/* checks the queue after the poll rate interval */
+TaskScheduler.prototype.checkQueueSoon = function() {
+    setTimeout(checkQueue.bind(this), this.tasker.options.pollRate);
 };
 
 /* returns whether our resources and options indicate that we should execute new tasks (true/false) */
@@ -93,11 +102,22 @@ TaskScheduler.prototype.getMemUsage = function() {
 };
 
 /* executes the given task, removing it from the queue */
-TaskScheduler.prototype.executeTask = function(task) {
+TaskScheduler.prototype.executeTask = function(task, checkNext, onDone) {
     
     /* this is the scheduler */
     var scheduler   = this,
         tasker      = scheduler.tasker;
     
-    //TODO: execute task
+    /* remove task from queue */
+    if (tasker.queue.removeTask(task)) {
+        
+        /* task removed */
+        tasker.emit('taskRemoved', task);
+    }
+    
+    /* execute task */
+    task.cb.apply(this, checkNext, onDone);
+    
+    /* task started */
+    tasker.emit('taskStarted', task);
 };
