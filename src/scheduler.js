482a90fa5bfaa2f03cc68f8bf366f94dddee1523
tommedema <tommedema@gmail.com>
2011-09-23 23:01:26 +0200

    implemented first half of scheduler

--- src/scheduler.js
+++ src/scheduler.js
@@ -15,19 +15,66 @@ var TaskScheduler = exports.TaskScheduler = function(tasker) {
     this.tasker = tasker;
     
     /* set interval */
-    this.interval = setInterval(function() {
-        
-        /* check if queue is populated */
-        
-        /* check CPU, MEM and TasksRunning */
-        
-        /* call callback of first task */
-        
-        /* pass checkNext, onDone */
-        
-        /* clear interval until checkNext is called */
-        
-        /* update running tasks on onDone */
-        
-    }, tasker.options.pollRate);
+    this.interval = setInterval(this.checkQueue, tasker.options.pollRate);
+};
+
+/* checks the queue for possibly needed activity */
+TaskScheduler.prototype.checkQueue = function() {
+    
+    /* this is the scheduler */
+    var scheduler   = this,
+        tasker      = scheduler.tasker;
+    
+    /* check if queue is populated */
+    if (!tasker.getTasksQueued()) return;
+    
+    /* check CPU, MEM and TasksRunning */
+    if (!this.shouldExecTask()) return;
+    
+    /* call callback of first task, pass checkNext and onDone */
+    var task  = tasker.queue.getFromIndex(0);
+    //task.execute();
+    
+    /* pass checkNext, onDone */
+    
+    /* clear interval until checkNext is called */
+    
+    /* update running tasks on onDone */
+    
+};
+
+/* returns whether our resources and options indicate that we should execute new tasks (true/false) */
+TaskScheduler.prototype.shouldExecTask = function() {
+    
+    /* this is the scheduler */
+    var scheduler   = this,
+        tasker      = scheduler.tasker,
+        options     = tasker.options;
+    
+    /* check each resource */
+    if (options.maxCpu && getCpuUsage() > options.maxCpu) {
+        tasker.emit('maxCpu');
+        return false;
+    }
+    if (options.maxMem && getMemUsage() > options.maxMem) {
+        tasker.emit('maxMem');
+        return false;
+    }
+    if (options.maxTasks && tasker.getTasksRunning() > options.maxTasks) {
+        tasker.emit('maxTasks');
+        return false;
+    }
+    
+    /* we should execute new tasks */
+    return true;
+};
+
+/* returns cpu usage as a percentage of total processing power (0-100) */
+TaskScheduler.prototype.getCpuUsage = function() {
+    
+};
+
+/* returns memory usage as a percentage of total memory available (0-100) */
+TaskScheduler.prototype.getMemUsage = function() {
+    
 };
